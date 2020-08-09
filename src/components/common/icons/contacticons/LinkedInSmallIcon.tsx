81fae411e58c1c87e48cf28aca11431afcae859b
James Elder <james@elder-software.com>
2020-08-09 15:14:23 +1000

    animations added to footer components

--- src/components/common/icons/contacticons/LinkedInSmallIcon.tsx
+++ src/components/common/icons/contacticons/LinkedInSmallIcon.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { TiSocialLinkedinCircular } from 'react-icons/ti';
+import './index.css';
 
 const LinkedInSmallIcon: React.FC = () => (
   <a
     href="https://www.linkedin.com/in/james-elder-8a335119a/"
     target="_blank"
     rel="noopener noreferrer">
-    <TiSocialLinkedinCircular
-      style={styles.linkedInIconStyle}
-    />
+    <div className={'iconContainer'}>
+      <TiSocialLinkedinCircular className={'liIcon'}/>
+    </div>
   </a>
 );
-
-const styles = {
-  linkedInIconStyle: {
-    flex: 1,
-    height: 40,
-    width: 40,
-    backgroundColor: 'black',
-    color: 'white',
-  } as React.CSSProperties
-}
 
 export { LinkedInSmallIcon };
