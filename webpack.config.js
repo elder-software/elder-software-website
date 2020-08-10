b9c6d66a3fe9099c7a950c1187aff9ee70d602ae
James Elder <james@elder-software.com>
2020-08-11 07:30:30 +1000

    built using webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
