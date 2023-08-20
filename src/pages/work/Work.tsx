3d06459d78a30283dd319436a28680dc910f2801
James Elder <james@elder-software.com>
2023-08-21 08:51:36 +1000

    added mylearners info

--- src/pages/work/Work.tsx
+++ src/pages/work/Work.tsx
@@ -15,29 +15,28 @@ const Work: React.FC = () => {
     /\.(png|jpe?g|svg)$/
   );
 
-  const gridItems = workInfo.map((item) => (
-    <WorkRow
-      key={item.name}
-      name={item.name}
-      icon={iconImages(item.icon)}
-      technologies={item.technologies}
-      description={item.description}
-      screenshots={item.screenshots.map((image) => screenShotImages(image))}
-      testimonial={item.testimonial}
-      testimonialAuthor={item.testimonialAuthor}
-    />
-  ));
-
-  const staggeredGridStyle = {
-    margin: 15,
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
-    gap: '15px',
-  };
-
   return (
-    <div style={staggeredGridStyle}>
-      {gridItems}
+    <div
+      style={{
+        margin: 15,
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        gap: '15px'
+      }}
+    >
+      {workInfo.map((item) => (
+        <WorkRow
+          key={item.name}
+          name={item.name}
+          icon={iconImages(item.icon)}
+          technologies={item.technologies}
+          keyDetails={item.keyDetails}
+          description={item.description}
+          screenshots={item.screenshots.map((image) => screenShotImages(image))}
+          testimonial={item.testimonial}
+          testimonialAuthor={item.testimonialAuthor}
+        />
+      ))}
     </div>
   );
 };
--- src/pages/work/WorkInfo.tsx
+++ src/pages/work/WorkInfo.tsx
@@ -10,6 +10,7 @@ export interface WorkInfoProps {
   name: string;
   icon: string;
   technologies: React.ReactNode[];
+  keyDetails: string[];
   description: string;
   screenshots: string[];
   testimonial?: string;
@@ -25,11 +26,11 @@ export const workInfo: WorkInfoProps[] = [
       <TvIcon key="tv" />,
       <MobileIcon key="mobile" />
     ],
+    keyDetails: [''],
     description:
       'The ABC is a free video streaming service for Australians. Watch Live Streams, TV shows and movies.' +
       '\n\nFrom a software standpoint it is an interesting app to work on requiring features to be developed ' +
-      'on both TV and Mobile platforms. We used many of the latest tools available to developers, ' +
-      'including Jetpack Compose, Arrow, Coroutines.',
+      'on both TV and Mobile platforms. We used many of the latest tools available to developers.',
     screenshots: [
       './iViewScreenShot1.jpg',
       './iViewScreenShot2.jpg',
@@ -46,12 +47,38 @@ export const workInfo: WorkInfoProps[] = [
       <AndroidIcon key="test" size={80} />,
       <MobileIcon key="mobile" />
     ],
+    keyDetails: [''],
     description:
-      'Virgin Australias official mobile app for assisting travellers.',
+    'Virgin Australias official mobile app for assisting travellers, allowing them to book and ' + 
+    'change flights, view boarding passes. The codebase used the latest recommended libraries' + 
+    ' Google for Android development regarding architecture and 3rd party libraries',
     screenshots: [
       './virginScreenshot1.jpg',
       './virginScreenshot2.jpg',
       './virginScreenshot3.jpg'
+    ],
+    testimonial: '',
+    testimonialAuthor: ''
+  },
+  {
+    name: 'myLearners',
+    icon: './mylearners.jpeg',
+    technologies: [
+      <AndroidIcon key="test" size={80} />,
+      <MobileIcon key="mobile" />
+    ],
+    keyDetails: [
+      '100,000K+ downloads',
+    ],
+    description:
+    'myLearners is a mobile app for assisting learner drivers in Australia. We worked with the government ' +
+    'of the state of Victoria to develop an application that can be used to track the users hours driven, road conditions ' + 
+    'and provide useful learning materials to help learner drivers move on to their full license.',
+    screenshots: [
+      './mylearnersScreenshot1.jpg',
+      './mylearnersScreenshot2.jpg',
+      './mylearnersScreenshot3.jpg',
+      './mylearnersScreenshot4.jpg',
     ],
     testimonial: '',
     testimonialAuthor: ''
