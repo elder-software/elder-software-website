35209ab2231b80636f2e390092031ff53962b388
James Elder <james@elder-software.com>
2025-04-03 13:59:22 +0200

    headers migrated to tailwind

--- src/components/icons/headerIcons/ContactIcon.tsx
+++ src/components/icons/headerIcons/ContactIcon.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { MdEmail } from 'react-icons/md';
 import { HeaderIconProps } from './styles';
-import './index.css';
 import Link from 'next/link';
 
 const ContactIcon: React.FC<HeaderIconProps> = ({ size, textSize }) => (
   <Link href="/contact">
-    <div className="iconBackground">
+    <div
+      className={`
+        group flex flex-col items-center content-center text-center
+        bg-black rounded px-2.5 py-1
+        transition-colors duration-500 hover:bg-white
+      `}
+    >
       <MdEmail
-        className="icon"
-        style={{
-          height: size || 80,
-          width: size || 80
-        }}
+        className={`
+          icon self-center text-white
+          transition-colors duration-500 group-hover:text-black
+        `}
+        style={{ height: size || 80, width: size || 80 }}
       />
-      <h5 className="iconText" style={{ fontSize: textSize }}>
+      <h5
+        className={`
+          iconText m-0 p-0 border-0 text-white
+          transition-colors duration-500 group-hover:text-black
+        `}
+        style={{ fontSize: textSize }}
+      >
         Contact
       </h5>
     </div>
--- src/components/icons/headerIcons/HomeIcon.tsx
+++ src/components/icons/headerIcons/HomeIcon.tsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { FaUser } from "react-icons/fa";
+import { FaHome, FaUser } from "react-icons/fa";
 import { HeaderIconProps } from "./styles";
-import "./index.css";
 import Link from "next/link";
 
 const HomeIcon: React.FC<HeaderIconProps> = ({ size, textSize }) => (
   <Link href="/">
-    <div className="iconBackground">
-      <FaUser
-        className="icon"
-        style={{
-          height: size || 80,
-          width: size || 80,
-        }}
+    <div
+      className={`
+        group flex flex-col items-center content-center text-center
+        bg-black rounded px-2.5 py-1
+        transition-colors duration-500 hover:bg-white
+      `}
+    >
+      <FaHome
+        className={`
+          icon self-center text-white
+          transition-colors duration-500 group-hover:text-black
+        `}
+        style={{ height: size || 80, width: size || 80 }}
       />
-      <h5 className="iconText" style={{ fontSize: textSize }}>
+      <h5
+        className={`
+          iconText m-0 p-0 border-0 text-white
+          transition-colors duration-500 group-hover:text-black
+        `}
+        style={{ fontSize: textSize }}
+      >
         Home
       </h5>
     </div>
